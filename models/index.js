6fdb1bb082d8ceb1b15b499fb29659271550505b
Swetha Muralidharan <swethamuralidharan@Swethas-MacBook-Pro.local>
2018-02-21 12:08:42 -0800

    auth check performed

---
+++ models/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/react_app");
+
+module.exports.connect = (uri) => {
+  mongoose.connect(uri);
+  // plug in the promise library:
+  mongoose.Promise = global.Promise;
+
+
+  mongoose.connection.on('error', (err) => {
+    console.error(`Mongoose connection error: ${err}`);
+    process.exit(1);
+  });
+
+  // load models
+  require('./user');
+};
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const config = require('./config');
+
+// connect to the database and load models
+require('.//models').connect(config.dbUri);
+
+const app = express();
+// tell the app to look for static files in these directories
+app.use(express.static('./static/'));
+// app.use(express.static('./client/dist/'));
+// tell the app to parse HTTP body messages
+app.use(bodyParser.urlencoded({ extended: false }));
+// pass the passport middleware
+app.use(passport.initialize());
+
+// load passport strategies
+const localSignupStrategy = require('./passport/local-signup');
+const localLoginStrategy = require('./passport/local-login');
+passport.use('local-signup', localSignupStrategy);
+passport.use('local-login', localLoginStrategy);
+
+// pass the authorization checker middleware
+const authCheckMiddleware = require('./middleware/auth-check');
+app.use('/api', authCheckMiddleware);
+
+// routes
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "PUT,GET,POST,DELETE");
+  next();
+});
+const authRoutes = require('./routes/auth');
+const apiRoutes = require('./routes/api');
+app.use('/auth', authRoutes);
+app.use('/api', apiRoutes);
+
+
+// start the server
+app.listen(8080, () => {
+  console.log('Server is running on http://localhost:8080 or http://127.0.0.1:8080');
+});
