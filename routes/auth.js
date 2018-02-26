2ffb9e1046efb426af1094539b3e421dc9a66751
Swetha Muralidharan <swethamuralidharan@Swethas-MacBook-Pro.local>
2018-02-26 14:58:31 -0800

    signup and login validations done

--- routes/auth.js
+++ routes/auth.js
@@ -89,7 +89,10 @@ router.post('/signup', (req, res, next) => {
   }
   return passport.authenticate('local-signup', (err) => {
     if (err) {
-      if (err.name === 'MongoError' && err.code === 11000) {
+      console.log("signup errors",err);
+      console.log("error name",err.name);
+      console.log("error code",err.code);
+      if ((err.name === 'MongoError' || err.name==='BulkWriteError') && err.code === 11000) {
         console.log(err.name);
         console.log(err.code);
         // the 11000 Mongo code is for a duplication email error
@@ -102,10 +105,10 @@ router.post('/signup', (req, res, next) => {
           }
         });
       }
-      return res.status(400).json({
-        success: false,
-        message: 'Could not process the form.'
-      });
+      // return res.status(400).json({
+      //   success: false,
+      //   message: 'Could not process the form.'
+      // });
     }
     return res.status(200).json({
       success: true,
